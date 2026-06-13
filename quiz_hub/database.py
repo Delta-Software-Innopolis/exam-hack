e5d7bdd578322017af776690c9454ffa9e213b47
fenton <viktorken8264@gmail.com>
2026-06-13 16:05:00 +0300

    feat: add sqlalchemy configuration for async database

---
+++ quiz_hub/database.py
@@ -0,0 +1,21 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from .config import DATABASE_URL
+from sqlalchemy.orm import DeclarativeBase
+from collections.abc import AsyncGenerator
+
+async_engine = create_async_engine(DATABASE_URL, echo=True) #type: ignore
+
+async_session_maker = async_sessionmaker(async_engine, expire_on_commit=False, class_=AsyncSession)
+
+class Base(DeclarativeBase):
+    pass
+
+
+async def get_async_db() -> AsyncGenerator[AsyncSession, None]:
+    """
+    Get access to the async sqlalchemy session
+    """
+
+    async with async_session_maker() as session:
+
+        yield session
