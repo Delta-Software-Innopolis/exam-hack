1965aed02450db51b3d2989c594435e82ba6aec4
fenton <viktorken8264@gmail.com>
2026-06-16 21:27:35 +0300

    feat: add store for user

---
+++ frontend/src/stores/user.ts
@@ -0,0 +1,9 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useUserStore = defineStore('user', () => {
+  const name = ref("")
+  const password = ref("")
+
+  return { name, password }
+})
