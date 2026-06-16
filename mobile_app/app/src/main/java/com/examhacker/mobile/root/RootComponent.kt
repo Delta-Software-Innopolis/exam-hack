846fa33519af5d60610fabc9168be04b5abb4309
eudok <eudokimovaaa@gmail.com>
2026-06-16 18:15:32 +0300

    feat: add authentication root navigation

--- mobile_app/app/src/main/java/com/examhacker/mobile/root/RootComponent.kt
+++ mobile_app/app/src/main/java/com/examhacker/mobile/root/RootComponent.kt
@@ -4,29 +4,52 @@ import com.arkivanov.decompose.router.stack.ChildStack
 import com.arkivanov.decompose.router.stack.childStack
 import com.arkivanov.decompose.router.stack.StackNavigation
 import com.arkivanov.decompose.value.Value
-
 import com.arkivanov.decompose.ComponentContext
 
-class RootComponent (private val componentContext: ComponentContext)
-    : ComponentContext by componentContext, IRootComponent {
+import kotlinx.serialization.Serializable
+
+import com.examhacker.authentication.component.AuthenticationComponent
+import com.examhacker.authentication.component.IAuthenticationComponent
+
+interface IRootComponent {
+
+    val stack: Value<ChildStack<*, Child>>
+
+    sealed class Child {
+        class Authentication(
+            val component: IAuthenticationComponent
+        ) : Child()
+    }
+}
+
+class RootComponent(private val componentContext: ComponentContext) :
+    ComponentContext by componentContext, IRootComponent {
 
     private val navigation = StackNavigation<Config>()
 
+    @Serializable
     sealed class Config {
-        data object Initial : Config() // temporary enter point
+        @Serializable
+        data object Authentication : Config()
     }
 
     override val stack: Value<ChildStack<*, IRootComponent.Child>> =
         childStack(
             source = navigation,
             serializer = null,
-            initialConfiguration = Config.Initial,
+            initialConfiguration = Config.Authentication,
             handleBackButton = true,
             childFactory = ::createChild,
         )
 
-    private fun createChild(config: Config, componentContext: ComponentContext) :
-            IRootComponent.Child {
-        TODO ("Children will be implemented later")
-    }
+    private fun createChild(
+        config: Config,
+        componentContext: ComponentContext,
+    ): IRootComponent.Child =
+        when (config) {
+            Config.Authentication ->
+                IRootComponent.Child.Authentication(
+                    AuthenticationComponent(componentContext)
+                )
+        }
 }
