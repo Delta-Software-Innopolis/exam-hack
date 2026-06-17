2b3dba58e771df857886a2d8b0b79a8f20886bd3
eudok <eudokimovaaa@gmail.com>
2026-06-17 14:50:53 +0300

    feat: implement root component navigation

--- mobile_app/app/src/main/java/com/examhacker/mobile/MainActivity.kt
+++ mobile_app/app/src/main/java/com/examhacker/mobile/MainActivity.kt
@@ -4,24 +4,53 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.arkivanov.decompose.defaultComponentContext
+import com.arkivanov.decompose.extensions.compose.stack.Children
+import com.examhacker.authentication.ui.AuthenticationScreen
+import com.examhacker.mobile.root.IRootComponent
+import com.examhacker.mobile.root.RootComponent
 import com.examhacker.resources.ExamHackerMobileTheme
 
-class MainActivity : ComponentActivity() {
-
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        // Always create the root component outside Compose on the main thread
+        val root =
+            RootComponent(
+                componentContext = defaultComponentContext(),
+            )
+
         setContent {
             ExamHackerMobileTheme {
-                // TODO Add navigation here
+                Text("RootComponent initialized")
+            }
+        }
+    }
+
+    @Composable
+    private fun RootContent(component: RootComponent, modifier: Modifier = Modifier,) {
+        Children(stack = component.stack, modifier = modifier,) {
+            when (val child = it.instance) {
+
+                is IRootComponent.Child.Authentication ->
+                    AuthenticationScreen(component = child.component)
+
+                is IRootComponent.Child.AIInteractions -> {}
+                is IRootComponent.Child.QuizList -> {}
+                is IRootComponent.Child.QuizEdit -> {}
+                is IRootComponent.Child.QuizSolve -> {}
+                is IRootComponent.Child.Settings -> {}
             }
         }
     }
 }
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
--- mobile_app/app/src/main/java/com/examhacker/mobile/root/RootComponent.kt
+++ mobile_app/app/src/main/java/com/examhacker/mobile/root/RootComponent.kt
@@ -11,14 +11,31 @@ import kotlinx.serialization.Serializable
 import com.examhacker.authentication.component.AuthenticationComponent
 import com.examhacker.authentication.component.IAuthenticationComponent
 
+import com.examhacker.ai_interactions.component.AIGenerationComponent
+import com.examhacker.ai_interactions.component.IAIGenerationComponent
+
+import com.examhacker.quiz_edit.component.IQuizEditComponent
+import com.examhacker.quiz_list.component.IQuizListComponent
+import com.examhacker.quiz_solve.component.IQuizSolveComponent
+
+import com.examhacker.settings.component.ISettingsComponent
+
+import com.examhacker.quiz_edit.component.QuizEditComponent
+import com.examhacker.quiz_list.component.QuizListComponent
+import com.examhacker.quiz_solve.component.QuizSolveComponent
+
+import com.examhacker.settings.component.SettingsComponent
 interface IRootComponent {
 
     val stack: Value<ChildStack<*, Child>>
 
     sealed class Child {
-        class Authentication(
-            val component: IAuthenticationComponent
-        ) : Child()
+        class Authentication(val component: IAuthenticationComponent) : Child()
+        class AIInteractions(val component: IAIGenerationComponent) : Child()
+        class QuizList(val component: IQuizListComponent) : Child()
+        class QuizEdit(val component: IQuizEditComponent) : Child()
+        class QuizSolve(val component: IQuizSolveComponent) : Child()
+        class Settings(val component: ISettingsComponent) : Child()
     }
 }
 
@@ -27,18 +44,12 @@ class RootComponent(private val componentContext: ComponentContext)
 
     private val navigation = StackNavigation<Config>()
 
-    @Serializable
-    sealed class Config {
-        @Serializable
-        data object Authentication : Config()
-    }
-
     override val stack: Value<ChildStack<*, IRootComponent.Child>> =
         childStack(
             source = navigation,
             serializer = null,
             initialConfiguration = Config.Authentication,
-            handleBackButton = true,
+            handleBackButton = false,
             childFactory = ::createChild,
         )
 
@@ -49,5 +60,52 @@ class RootComponent(private val componentContext: ComponentContext)
                 IRootComponent.Child.Authentication(
                     AuthenticationComponent(componentContext)
                 )
+
+            Config.AIInteractions ->
+                IRootComponent.Child.AIInteractions(
+                    AIGenerationComponent(componentContext)
+                )
+
+            Config.QuizList ->
+                IRootComponent.Child.QuizList(
+                    QuizListComponent(componentContext)
+                )
+
+            Config.QuizEdit ->
+                IRootComponent.Child.QuizEdit(
+                    QuizEditComponent(componentContext)
+                )
+
+            Config.QuizSolve ->
+                IRootComponent.Child.QuizSolve(
+                    QuizSolveComponent(componentContext)
+                )
+
+            Config.Settings ->
+                IRootComponent.Child.Settings(
+                    SettingsComponent(componentContext)
+                )
         }
+    @Serializable
+    sealed class Config {
+
+        @Serializable
+        data object Authentication : Config()
+
+        @Serializable
+        data object AIInteractions : Config()
+
+        @Serializable
+        data object QuizList : Config()
+
+        @Serializable
+        data object QuizEdit : Config()
+
+        @Serializable
+        data object QuizSolve : Config()
+
+        @Serializable
+        data object Settings : Config()
+    }
+
 }
