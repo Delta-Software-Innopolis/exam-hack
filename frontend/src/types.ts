88d7fc5e8c944eaa12df270a7abf7494c5f18959
fenton <viktorken8264@gmail.com>
2026-06-19 16:44:06 +0300

    refactor: update QuizItem type

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,5 +1,9 @@
-interface QuizItem {
+export default interface QuizItem {
     id: number,
+    name: string,
+    creation_date: Date,
+    updation_date: Date,
+    forked_from: number|null,
     question: string,
     options: string[],
     correct: number,
