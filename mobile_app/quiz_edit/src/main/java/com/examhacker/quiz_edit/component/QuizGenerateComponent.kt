c461024eceaa72045c4e5febbfc9a9e7ee2240f2
eudok <eudokimovaaa@gmail.com>
2026-06-23 14:28:37 +0300

    feat: add quiz name screen ui

---
+++ mobile_app/quiz_edit/src/main/java/com/examhacker/quiz_edit/component/QuizGenerateComponent.kt
@@ -0,0 +1,20 @@
+package com.examhacker.quiz_edit.component
+
+import com.arkivanov.decompose.ComponentContext
+
+interface IQuizGenerateComponent {
+    val hasFiles: Boolean
+
+    fun addFile()
+}
+
+class QuizGenerateComponent(
+    componentContext: ComponentContext
+) : IQuizGenerateComponent, ComponentContext by componentContext {
+
+    override val hasFiles: Boolean = false
+
+    override fun addFile() {
+        // TODO
+    }
+}
