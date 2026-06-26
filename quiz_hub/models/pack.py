a1df9a922dd7358b0d5122abcb927527ee94537b
fenton <viktorken8264@gmail.com>
2026-06-26 17:35:52 +0300

    chore: add author_id field to pack model

--- quiz_hub/models/pack.py
+++ quiz_hub/models/pack.py
@@ -1,6 +1,6 @@
 from typing import TYPE_CHECKING
 
-from sqlalchemy import String, Text, DateTime
+from sqlalchemy import String, Text, DateTime, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from database import Base
 from datetime import datetime, timezone
@@ -18,6 +18,7 @@ class Pack(Base):
     name: Mapped[str] = mapped_column(String(50), nullable=False, index=True)
     creation_date: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
     updating_date: Mapped[datetime|None] = mapped_column(DateTime(timezone=True), default=None)
+    author_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
 
     published: Mapped["Published_pack | None"] = relationship("Published_pack", back_populates="source", uselist=False)
     cards: Mapped[list["Card"]] = relationship("Card", back_populates="pack")
