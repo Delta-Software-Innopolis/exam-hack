607cc3ea096e927952edcd9f91e5aa5f63d649cd
fenton <viktorken8264@gmail.com>
2026-07-06 20:56:19 +0300

    feat: add new route for suggestions

--- quiz_hub/main.py
+++ quiz_hub/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from routers.pack import router as pack_router
+from routers.suggestion import router as suggestion_router
 app = FastAPI(root_path="/hub")
 
 origins = [
@@ -9,6 +10,7 @@ origins = [
 ]
 
 app.include_router(pack_router)
+app.include_router(suggestion_router)
 
 app.add_middleware(
     CORSMiddleware,
