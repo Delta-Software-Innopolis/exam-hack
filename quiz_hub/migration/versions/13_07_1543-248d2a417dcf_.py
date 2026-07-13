5b326e0d2d0a19f2cdd3921c2bfbf55c780fe8be
fenton <viktorken8264@gmail.com>
2026-07-13 16:21:39 +0300

    fix: add cascade for constraint and handling of adding dublication packs

---
+++ quiz_hub/migration/versions/13_07_1543-248d2a417dcf_.py
@@ -0,0 +1,87 @@
+"""empty message
+
+Revision ID: 248d2a417dcf
+Revises: cadec56c693e
+Create Date: 2026-07-13 15:43:50.331655
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.engine.reflection import Inspector
+
+# revision identifiers, used by Alembic.
+revision: str = '248d2a417dcf'
+down_revision: Union[str, Sequence[str], None] = 'cadec56c693e'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+bind = op.get_bind()
+inspector = Inspector.from_engine(bind)
+
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    if inspector.has_table("pack_permissions"):
+        op.drop_constraint(
+            "pack_permissions_user_id_fkey",
+            "pack_permissions",
+            type_="foreignkey",
+        )
+
+        op.drop_constraint(
+            "pack_permissions_pack_id_fkey",
+            "pack_permissions",
+            type_="foreignkey",
+        )
+
+        op.create_foreign_key(
+            "pack_permissions_user_id_fkey",
+            "pack_permissions",
+            "users",
+            ["user_id"],
+            ["id"],
+            ondelete="CASCADE",
+        )
+
+        op.create_foreign_key(
+            "pack_permissions_pack_id_fkey",
+            "pack_permissions",
+            "packs",
+            ["pack_id"],
+            ["id"],
+            ondelete="CASCADE",
+        )
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    if inspector.has_table("pack_permissions"):
+        op.drop_constraint(
+            "pack_permissions_user_id_fkey",
+            "pack_permissions",
+            type_="foreignkey",
+        )
+
+        op.drop_constraint(
+            "pack_permissions_pack_id_fkey",
+            "pack_permissions",
+            type_="foreignkey",
+        )
+
+        op.create_foreign_key(
+            "pack_permissions_user_id_fkey",
+            "pack_permissions",
+            "users",
+            ["user_id"],
+            ["id"],
+        )
+
+        op.create_foreign_key(
+            "pack_permissions_pack_id_fkey",
+            "pack_permissions",
+            "packs",
+            ["pack_id"],
+            ["id"],
+        )
