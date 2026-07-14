8867b85d2e7a50579151de0486960d4e1d667694
Timur Chumaraev <chumarayev@bk.ru>
2026-07-14 22:33:02 +0300

    Feat/178 delete quiz frontend (#193)
    * feat(frontend): add deleting quizzes
    
    * refactor(frontend): QuizCreationView.vue
    
    * fix: Database optimizations for quiz core service
    
    * fix(frontend): add file extencion and size restrictions on upload
    
    * feat: use new endpoint on create quiz
    
    * feature: Add settings UI and basic logic
    
    * feature: Add informative snack bar for not implemented features
    
    * feat: add descrition and cards field for GET endpoint
    
    * fix: Add description field in pack model
    
    * fix: add cascade for constraint and handling of adding dublication packs
    
    * fix: add CASCADE for cards and options
    
    * fix(migration): move bind and inspector to function body and change down revision
    
    * feat(frontend): add deleting quizzes
    
    * fix(frontend): duplicated code
    
    ---------
    
    Co-authored-by: Stepan Tarabin <upconett@gmail.com>
    Co-authored-by: fenton <viktorken8264@gmail.com>
    Co-authored-by: ustkost <q2zoff.jr@gmail.com>

--- frontend/src/core.ts
+++ frontend/src/core.ts
@@ -139,3 +139,18 @@ export async function deleteCards(cardIds: number[]): Promise<boolean> {
         return false
     }
 }
+
+export async function deletePack(packId: number): Promise<boolean> {
+    const nm = useNetworkManager()
+
+    try {
+        const response = await nm.fetch_core(`/core/pack/${packId}`, {
+            method: "DELETE"
+        })
+
+        return response.ok
+    } catch (err) {
+        console.error(err)
+        return false
+    }
+}
